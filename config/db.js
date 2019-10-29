fdbd8e24a2049940e4a474a20d220622aa1f36f7
mkibilko <mateusz.kibilko@gmail.com>
2019-10-29 14:44:11 +0100

    add CRUD operations for bootcamp

--- config/db.js
+++ config/db.js
@@ -1,17 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI, 
-    {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true
     });
 
-    if(conn) {
-        console.log(`Database connect to host ${conn.connection.host}`);
-    }
-}
+    console.log(`Database connect to ${conn.connection.host}`.underline.cyan);
+};
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const colors = require('colors');
-const connectDB = require('./config/db');
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -14,6 +14,9 @@ const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
@@ -22,11 +25,11 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
     )
 );
 
-// process.on('unhandledRejection', (err, promise) => {
-//     console.error(`Error: ${err.message}`.red);
-//     server.close(() => process.exit(1));
-// })
+process.on("unhandledRejection", (err, promise) => {
+    console.error(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+});
